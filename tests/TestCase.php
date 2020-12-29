837c88479062e2556780d5bc8559979b0e2f0822
Christian Griffin <c.griffin@vehikl.com>
2020-12-29 16:17:25 -0500

    add happy path conversion tests

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -6,5 +6,13 @@ use PHPUnit\Framework\TestCase as PhpUnitTestCase;
 
 class TestCase extends PhpUnitTestCase
 {
+    protected function getFixturePath(string $file): string
+    {
+        return __DIR__ . '/fixtures/' . $file;
+    }
 
+    protected function getFixtureContents(string $file): string
+    {
+        return trim(file_get_contents($this->getFixturePath($file)));
+    }
 }
---
+++ tests/Unit/MidiTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace MidiConverter\Tests\Unit;
+
+use Midi;
+use MidiConverter\Tests\TestCase;
+
+class MidiTest extends TestCase
+{
+    public function testItConvertsAMidiFileToAnXmlFile(): void
+    {
+        $midi = new Midi();
+        $midi->importMid($this->getFixturePath('daughter_of_the_sea.midi'));
+
+        $this->assertEquals($this->getFixtureContents('daughter_of_the_sea.xml'), trim($midi->getXml(1)));
+    }
+
+    public function testItConvertsAMidiFileToATxtFile(): void
+    {
+        $midi = new Midi();
+        $midi->importMid($this->getFixturePath('daughter_of_the_sea.midi'));
+
+        $this->assertEquals($this->getFixtureContents('daughter_of_the_sea.txt'), trim($midi->getTxt(1)));
+    }
+}
