6762c12f92578bc9bc22e32eb199ee0e7d7c124e
Christian Griffin <c.griffin@vehikl.com>
2020-12-29 18:04:26 -0500

    test reverse file format

--- tests/Unit/MidiTest.php
+++ tests/Unit/MidiTest.php
@@ -22,4 +22,42 @@ class MidiTest extends TestCase
 
         $this->assertEquals($this->getFixtureContents('daughter_of_the_sea.txt'), trim($midi->getTxt(1)));
     }
+
+    public function testItConvertsAnXmlFileToAMidiFile(): void
+    {
+        $midi = new Midi();
+        $midi->importXml($this->getFixtureContents('daughter_of_the_sea.xml'));
+        $fileHandler = $this->writeTemporaryFile($midi->getMid());
+
+        $midi = new Midi();
+        $midi->importMid($this->getTemporaryFilePath($fileHandler));
+
+        $this->assertEquals($this->getFixtureContents('daughter_of_the_sea.xml'), trim($midi->getXml(1)));
+    }
+
+    public function testItConvertsATxtFileToAMidiFile(): void
+    {
+        $midi = new Midi();
+        $midi->importTxt($this->getFixtureContents('daughter_of_the_sea.txt'));
+        $fileHandler = $this->writeTemporaryFile($midi->getMid());
+
+        $midi = new Midi();
+        $midi->importMid($this->getTemporaryFilePath($fileHandler));
+
+        $this->assertEquals($this->getFixtureContents('daughter_of_the_sea.txt'), trim($midi->getTxt(1)));
+    }
+
+    /** @return false|resource */
+    private function writeTemporaryFile(string $content)
+    {
+        $file = tmpfile();
+        fwrite($file, $content);
+        return $file;
+    }
+
+    /** @param false|resource $fileHandler */
+    private function getTemporaryFilePath($fileHandler): string
+    {
+        return stream_get_meta_data($fileHandler)['uri'] ?? '';
+    }
 }
